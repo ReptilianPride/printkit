c64b0e5216b546d0ae427bf747e12a4091c2cbae
CipherKill <37742577+CipherKill@users.noreply.github.com>
2022-06-08 18:41:21 +0530

    Fixed setup

--- printkit/__init__.py
+++ printkit/__init__.py
@@ -7,7 +7,7 @@ import os
 os.system('')
 
 def version():
-    print('Current Version: 0.1.2')
+    print('Current Version: 0.1.3')
 
 def colors():
     availableColors=[
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 DESCRIPTION = 'Printkit is an easy to use python package which contains various tools that enable you to have a distinct and good looking outputs.'
 
 # Setting up
